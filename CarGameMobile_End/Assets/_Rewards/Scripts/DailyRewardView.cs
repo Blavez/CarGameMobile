860b164027db9ec3931a6b1dda7d38352ae1d14a
Blavez <76618921+Blavez@users.noreply.github.com>
2021-12-02 18:46:18 +0300

    Upload

--- CarGameMobile_End/Assets/_Rewards/Scripts/DailyRewardView.cs
+++ CarGameMobile_End/Assets/_Rewards/Scripts/DailyRewardView.cs
@@ -12,7 +12,7 @@ namespace Rewards
         private const string TimeGetRewardKey = nameof(TimeGetRewardKey);
 
         [field: Header("Settings Time Get Reward")]
-        [field: SerializeField] public float TimeCooldown { get; private set; } = 86400;
+        [field: SerializeField] public float TimeCooldown { get; private set; } = 604800;
         [field: SerializeField] public float TimeDeadline { get; private set; } = 172800;
 
         [field: Header("Settings Rewards")]
